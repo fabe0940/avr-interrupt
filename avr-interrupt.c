8337a69a966d6654ebbb88357bde03f9d729f606
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-02-22 15:40:43 -0800

    commenting

--- avr-interrupt.c
+++ avr-interrupt.c
@@ -11,7 +11,11 @@ extern volatile uint8_t cnt;
 int main(void) {
 	init();
 
+	/* Main loop */
 	while (1) {
+		/* Output the counter on the upper 4 LEDS
+ 		 * and the status of the switches on the lower 4 LEDS
+ 		 */
 		PORTB = ~((cnt << 4) | (~PINA & 0x0F));
 	}
 	
@@ -19,11 +23,14 @@ int main(void) {
 }
 
 void init(void) {
+	/* Initialize IO registers */
 	DDRA = 0x00;
 	DDRB = 0xFF;
 
+	/* Initialize subsystems */
 	timerInit();
 
+	/* Enable interrupts */
 	sei();
 
 	return;
--- timer.c
+++ timer.c
@@ -6,16 +6,22 @@ extern volatile uint8_t cnt;
 volatile uint8_t overflowCnt;
 
 void timerInit(void) {
+	/* Initialize TIMER0 to use the overflow ISR */
 	TCCR0 = (TCCR0 & ~PRESCALER_MASK) | PRESCALER_256;
 	TIMSK |= (1 << TOIE0);
 	TCNT0 = 0x00;
 
+	/* Initialize subsystem values */
 	cnt = 0;
 	overflowCnt = 0;
 
 	return;
 }
 
+/* TIMER0 Overflow ISR */
 ISR(TIMER0_OVF_vect) {
+	/* Increment overflowCnt. Additionally, increment cnt if overflowCnt is
+	 * a multiple of OVERFLOWS_PER_SECOND
+	 */
 	if (!(++overflowCnt % OVERFLOWS_PER_SECOND)) cnt++;
 }
